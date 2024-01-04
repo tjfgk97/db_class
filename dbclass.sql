28b59c9de8aafe20b1281963f6b06416371c4a3e
tjfgk97 <tjfgk97@gmail.com>
2024-01-04 13:23:55 +0900

    select 예제 완.

--- dbclass.sql
+++ dbclass.sql
@@ -504,6 +504,7 @@ insert into parent3(id, p1, p2) values(3, 'cc', 'cc');
 insert into parent3(id, p1, p2) values(4, 'dd', 'dd');
 select * from parent3;
 
+
 insert into child3(id, c1, c2, p_id) values(1, 'aaa', 'aaa', 1);
 insert into child3(id, c1, c2, p_id) values(2, 'bbb', 'bbb', 2);
 insert into child3(id, c1, c2, p_id) values(3, 'ccc', 'ccc', 3);
@@ -572,3 +573,28 @@ insert into orders(customer_id, book_id, o_saleprice, o_orderdate) values(3, 8, 
 select * from book;
 select * from customer;
 select * from orders;
+
+-- 1. 모든 도서의 가격과 도서명 조회 
+select b_price, b_bookname from book;
+-- 2. 모든 출판사 이름 조회 
+select b_publisher from book;
+-- 2.1 중복값을 제외한 출판사 이름 조회 
+select distinct b_publisher from book;
+-- 3. BOOK테이블의 모든 내용 조회 
+select * from book;
+-- 4. 20000원 미만의 도서만 조회 
+select * from book where b_price < 20000;
+-- 5. 10000원 이상 20000원 이하인 도서만 조회
+select * from book where b_price >= 10000 AND b_price <=20000;
+-- 6. 출판사가 좋은출판사 또는 대한출판사인 도서 조회 
+select * from book where b_publisher = '좋은출판사' OR b_publisher = '대한출판사';
+-- 7. 도서명에 축구가 포함된 모든 도서를 조회
+select * from book where b_bookname like '%축구%';
+-- 8. 도서명의 두번째 글자가 구인 도서 조회
+select * from book where b_bookname like '_구%';
+-- 9. 축구 관련 도서 중 가격이 20000원 이상인 도서 조회
+select * from book where b_bookname like '%축구%' AND b_price >= 20000;
+-- 10. 책 이름순으로 전체 도서 조회
+select * from book order by b_bookname asc;
+-- 11. 도서를 가격이 낮은 것 부터 조회하고 같은 가격일 경우 도서명을 가나다 순으로 조회
+select * from book order by b_price asc, b_bookname asc;
